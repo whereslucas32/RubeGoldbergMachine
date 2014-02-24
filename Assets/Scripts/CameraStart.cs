1275d20d03e64c1aa6b2ba3a435928427535e26c
Raelle Richard <doodlecat2000@yahoo.com>
2014-02-24 10:31:45 -0500

    More homework....

---
+++ Assets/Scripts/CameraStart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraStart : MonoBehaviour {
+
+	public GameObject Camera2, Camera3;
+	void Start () {
+		gameObject.camera.enabled = true;
+		Camera2.camera.enabled = false;
+		Camera3.camera.enabled = false;
+	}
+}
---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour {
+
+	public GameObject Camera1, Camera2;
+	void OnTriggerEnter(){
+		Camera1.camera.enabled = false;
+		Camera2.camera.enabled = true;
+	}
+}
